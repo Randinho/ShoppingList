be0f9b36e01289ff6aff9131442674aca2118960
Randinho <kamil.plastik@wp.pl>
2020-12-03 18:35:04 +0100

    Initial commit

---
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.shoppinglist;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "TAG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        User user = new User("Login", "Password");
+
+        db.collection("users").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+    }
+}
---
+++ app/src/main/java/com/example/shoppinglist/User.java
@@ -0,0 +1,12 @@
+package com.example.shoppinglist;
+
+public class User {
+    public String Login;
+    public String Password;
+
+    public User(String login, String password){
+        Login = login;
+        Password = password;
+    }
+
+}
